397d0d3d0d51a161bfd7ab5985ee9759512f6f7b
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-03-26 22:12:35 +0000

    Added address foreign key to customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326213059) do
+ActiveRecord::Schema.define(version: 20170326215559) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20170326213059) do
     t.string   "password"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "address_id"
+    t.index ["address_id"], name: "index_customers_on_address_id"
   end
 
   create_table "order_items", force: :cascade do |t|
