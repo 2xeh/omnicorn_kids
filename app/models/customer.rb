415a87b399724979b7ed55b6186831b569c50fc4
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-03-29 15:41:10 +0000

    edited files per rubocop suggestions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
-  # note: for now, one customer has one address. However, this doesn't mean that two customers can't have the same address
+  # note: for now, one customer has one address.
+  # However, this doesn't mean that two customers can't have the same address
   # if the billing address is different, that is handled in the payment table
   belongs_to :address
   has_many :orders
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,8 @@ class Order < ApplicationRecord
   belongs_to :address
   belongs_to :customer
 
-  # note: it is worth considering that a user might use multiple payments for an order. Like a gift certificate or two credit cards
+  # note: it is worth considering that a user might use
+  # multiple payments for an order. Like a gift certificate or two credit cards
   has_many :payments
   has_many :order_items
 end
