779b853c8b45e1f62f3922ed5e3dafb63cc5fff8
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-04-05 23:42:18 +0000

    added controllers and views for order items and carts - for shopping cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin2@example.com', password: 'password',\
+
+AdminUser.delete_all
+AdminUser.create!(email: 'admin@example.com', password: 'password',\
                   password_confirmation: 'password')
+
+
+Product.delete_all
+Product.create! id: 1, name: "Banana", price: 0.49, active: true
+Product.create! id: 2, name: "Apple", price: 0.29, active: true
+Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
