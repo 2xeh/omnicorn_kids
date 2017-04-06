1d25788c2c30ade8728eec5c97c737f492db0b49
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-04-06 15:19:39 +0000

    disabled robots and set email for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,26 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+
+  # Enabling email in production for Heroku
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+
+  # ActionMailer config for Heroku
+  config.action_mailer.default_url_options = {
+    host: Rails.application.secrets.domain_name
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
