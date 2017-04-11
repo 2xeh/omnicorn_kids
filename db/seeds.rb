47d2afefa2fad553e17d25592ffb59355c694b10
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-04-11 21:29:42 +0000

    changed seeds file to ignore active admin data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.destroy_all
-AdminUser.create!(email: 'admin@example.com', password: 'password',\
-                  password_confirmation: 'password')
+# AdminUser.destroy_all
+# AdminUser.create!(email: 'admin@example.com', password: 'password',\
+#                   password_confirmation: 'password')
 
 OrderItem.destroy_all
 Order.destroy_all
